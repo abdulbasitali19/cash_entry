952e199e5ff7072c4441a44e0911db136e439732
abdulbasitali19 <abdulbasitalishaik1@gmail.com>
2024-04-22 11:09:56 +0000

    Cash entry form for creating journal entry

---
+++ cash_entry/cash_entry/doctype/cash_entry_form/cash_entry_form.js
@@ -0,0 +1,18 @@
+// Copyright (c) 2024, abdulbasitali and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Cash Entry Form', {
+	// refresh: function(frm) {
+
+	// }
+
+	setup(frm) {
+		frm.set_query("cashbank", function() {
+			return {
+				filters: [
+					['Account', 'account_type', 'in', 'Cash, Bank']
+				]
+			};
+		});
+	},
+});
